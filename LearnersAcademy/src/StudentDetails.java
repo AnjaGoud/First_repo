590423b5fff5db9427c4b94bad51fa2a1de97026
AnjaGoud <anjagoudgadipally96@gmail.com>
2022-08-29 07:39:02 +0530

    first commit

---
+++ LearnersAcademy/src/StudentDetails.java
@@ -0,0 +1,15 @@
+
+public class StudentDetails {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String studentName= "anji ";
+		String fName="narsa goud";
+		int rollNo=1;
+		  long  phn = 9666357138l;
+		  System.out.println( studentName + ""  +phn );
+		
+	}
+
+}
