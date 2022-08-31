caa265a87faf9a1de72dcf1d14300f81b85d656c
AnjaGoud <anjagoudgadipally96@gmail.com>
2022-08-31 08:42:46 +0530

    day 3

---
+++ Calculator/src/com/anjagoud/Addition.java
@@ -0,0 +1,12 @@
+package com.anjagoud;
+
+import java.util.Scanner;
+
+public class Addition {
+public static void main(String[] args) {
+	private final String userName ="root";
+			private final String password ="root12345";
+
+	}
+
+}
